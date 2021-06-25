8bbbd4b5b428d5d9c4e2cebe4d4a78729b948428
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-06-25 11:43:57 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,5 +1,10 @@
 s=input("Enter a string:")
-print("Vowels in the string are:")
+print("Vowels in the string are")
 for char in  s:
     if char=='a' or char=='e' or char=='i' or char=='o' or char=='u':
         print(char,end=" ")
+
+Output:
+Enter a string:Python Programming
+Vowels in the string are
+o o a i             
