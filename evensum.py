671d63b231f4749e220f51508b1830ffe8d93e2c
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-06-28 12:24:08 +0530

    Update evensum.py

--- evensum.py
+++ evensum.py
@@ -8,3 +8,13 @@ for i in a:
         print("i=",i,"and sum=",sum)
 print("Sum of even numbers:")
 print ("total=",sum)
+
+Output:
+range(0, 10)
+<class 'range'>
+i= 0 and sum= 0
+i= 2 and sum= 2
+i= 4 and sum= 6
+i= 6 and sum= 12
+i= 8 and sum= 20
+Sum of even numbers:20
