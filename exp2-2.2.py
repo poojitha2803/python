bc715061e94f689c45b37ecd15bd9279c38baa55
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-06-28 12:34:40 +0530

    Update exp2-2.2.py

--- exp2-2.2.py
+++ exp2-2.2.py
@@ -1,4 +1,18 @@
+2.2) Write a program that asks the user for their name and how many times to 
+print it. The program should print out the user’s name the specified number of
+times
+
+Program:
 name=str(input("Enter your name:"))
 n=int(input("Enter the value of n:"))
 for i in range(n):
     print(name)
+
+Output:
+Enter your name:Poojitha
+Enter the value of n:5
+Poojitha
+Poojitha
+Poojitha
+Poojitha
+Poojitha       
