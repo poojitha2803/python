7746a2827d72ce2bda3decd0cc47e77dca9bc815
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-06-28 12:32:27 +0530

    Update exp1-1.2.py

--- exp1-1.2.py
+++ exp1-1.2.py
@@ -1,3 +1,9 @@
+1.2) Write a program that asks the user to enter three numbers (use three
+separate input statements). Create variables called total and average that hold
+the sum and average of the three numbers and print out the values of total and
+average.
+
+Program:
 a=int(input("Enter a value:"))
 b=int(input("Enter b value:"))
 c=int(input("Enter c value:"))
@@ -5,3 +11,10 @@ t=(a+b+c)
 avg=t/3
 print("Total of the 3 numbers is:",t)
 print("Average of the 3 numbers is:",avg)
+
+Output:
+Enter a value:4
+Enter b value:5
+Enter c value:6
+Total of the 3 numbers is: 15
+Average of the 3 numbers is: 5.0
