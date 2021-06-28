568ebfa5ee7d46a572bade369fb9d9b5e373a48b
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-06-28 12:32:03 +0530

    Add files via upload

---
+++ exp1-1.2.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+c=int(input("Enter c value:"))
+t=(a+b+c)
+avg=t/3
+print("Total of the 3 numbers is:",t)
+print("Average of the 3 numbers is:",avg)
