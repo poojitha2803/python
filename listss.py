9f96c226a4d1953a1316620ad0fc2b77dde1247b
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-06-28 12:25:30 +0530

    Update listss.py

--- listss.py
+++ listss.py
@@ -5,3 +5,11 @@ print("Size of list:",len(l))
 print("Maximum value of list:",max(l))
 print("Minimum value of list:",min(l))
 print(l[:3])
+
+Output:
+[400, 678, 2, -1, 1438909]
+2
+Size of list: 5
+Maximum value of list: 1438909
+Minimum value of list: -1
+[400, 678, 2]  
