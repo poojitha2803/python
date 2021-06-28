77ed5a95ce0d0906a4c6815ad0c19a5d2198e9f1
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-06-28 12:30:39 +0530

    Update exp1-1.1.py

--- exp1-1.1.py
+++ exp1-1.1.py
@@ -1,3 +1,13 @@
-k=float(input("Enter the value in kilos"))
+
+1. Basic Programs
+1.1) Write a program that asks the user for a weight in kilograms and converts it
+to pounds. There are 2.2 pounds in a kilogram.
+
+Program:
+k=float(input("Enter the value in kilos:"))
 p=(2.2)*k
 print("%0.3f in kilos is equal to %0.3f in pounds"%(k,p))
+
+Output:
+Enter the value in kilos:45
+45.000 in kilos is equal to 99.000 in pounds
